4f3415013ff5bb70306d4a47fb15b5e2beba13e6
Ola <ola@olavea.com>
2022-09-20 10:45:39 +0200

    remove code instead of commenting it out

--- packages/gatsby-transformer-cloudinary/gatsby-node.js
+++ packages/gatsby-transformer-cloudinary/gatsby-node.js
@@ -3,11 +3,7 @@ const {
   createCloudinaryAssetType,
   createCloudinaryAssetNodes,
 } = require('./node-creation');
-// const {
-//   createGatsbyImageResolvers,
-//   addGatsbyImageFragments,
-//   createGatsbyImageTypes,
-// } = require('./gatsby-image');
+
 const { createGatsbyImageDataResolver } = require('./gatsby-plugin-image');
 
 let coreSupportsOnPluginInit = undefined;
